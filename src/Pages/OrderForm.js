32c1c8cbdcd6aebce50f592f5b5f2e6ea9ebe075
Scovia Achan <achanscovia@gmail.com>
2021-06-20 20:25:48 +0300

    Add signup endpoint

--- src/Pages/OrderForm.js
+++ src/Pages/OrderForm.js
@@ -6,20 +6,20 @@ export default function OrderForm() {
     <div className="Parcel">
       <div className="order">
         <h2 style={{ textAlign: "center", color: "white" }}>
-            Create order
+            Send a parcel
         </h2>
         <form className="order-form">
           
           <label>Type of item</label>
-          <input type="text" placeholder="item type" />
-          <label>Last name</label>
-          <input type="text" placeholder="Last Name" />
-          <label>Email</label>
-          <input type="email" placeholder="name@example.com" />
-          <label>Password</label>
-          <input type="email" placeholder="password" />
+          <input type="text" placeholder="item name" />
+          <label>Weight</label>
+          <input type="text" placeholder="weight" />
+          <label>Destination</label>
+          <input type="text" placeholder="where to be delivered" />
+          <label>Pickup Location</label>
+          <input type="text" placeholder="Where is the parcel" />
           <label>confirm Password</label>
-          <input type="email" placeholder="reapeat password" />
+          <input type="text" placeholder="reapeat password" />
           <button className="auth-btn">Submit order</button>
          
         </form>
